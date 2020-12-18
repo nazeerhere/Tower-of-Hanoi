e547146b098e45457971d278fa1d4b8a30275e32
nazeer171 <nazeer3886@gmail.com>
2020-12-18 00:21:06 -0600

    added a disc, and some shady colors

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("This works!check check")
+
+const demoDisc = document.querySelector(".demoDisc");
+const hanoi = document.querySelector(".hanoi")
+const discScreenId2 = document.querySelector("#DScreen2")
+
+
+const playThing = document.getElementById("playThing")
+playThing.style.background = "pink"
+
